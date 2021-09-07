e81213f0c3e21d0dbe4b16c451d39d81868b0e61
Geremias Emerim <geremiasse@gmail.com>
2021-09-07 18:57:57 -0300

    projetos criados

---
+++ backend/src/main/java/com/ge/gevendas/GevendasApplication.java
@@ -0,0 +1,13 @@
+package com.ge.gevendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GevendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GevendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ge/gevendas/GevendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ge.gevendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GevendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
