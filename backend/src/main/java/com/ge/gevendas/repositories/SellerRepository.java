92a6a862135bb68013644c5c90deb42c878bc616
Geremias Emerim <geremiasse@gmail.com>
2021-09-09 21:53:59 -0300

    Layers

---
+++ backend/src/main/java/com/ge/gevendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.ge.gevendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ge.gevendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long> {
+
+}
